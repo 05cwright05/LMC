3eb906a131a9fcbbd6e5fd0b341bd6a363812373
Caden <05cwright05@gmail.com>
2025-01-21 15:59:39 -0500

    Developed a script to process all videos in folder at once, and added support for linux / wsl

---
+++ bin/process_videos.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Processing all files in 'videos' folder"
+for filename in videos/*; do
+    echo "Processing: $filename"
+    python3 capture_frames.py "$filename"
+done
--- capture_frames.py
+++ capture_frames.py
@@ -1,9 +1,14 @@
+#Need to alter this so that it does evert TR instead of every frame 
 import cv2 
 import sys
 from pathlib import Path
+import os
 
 # Function to extract frames 
 def FrameCapture(path): 
+
+    #define TR as 1.5 seconds
+    TR = 1.5
 
     # Path to video file 
     vidObj = cv2.VideoCapture(path) 
@@ -15,9 +20,15 @@ def FrameCapture(path):
 
     # track num frames
     num_frames = 0
+    #track what TR we are on
+    current_TR = 0
 
     #create a folder name for this video
-    path_header = "processed/" + path.split("\\")[1].split(".")[0]
+    if os.name == 'nt':  #windows
+        path_header = "processed\\" + path.split("\\")[1].split(".")[0]
+    else: #linux
+        print("ran")
+        path_header = "processed/" + path.split("/")[1].split(".")[0]
 
     #generate that folder
     Path(path_header).mkdir(parents=True, exist_ok=True)
@@ -25,18 +36,37 @@ def FrameCapture(path):
     # checks whether frames were extracted 
     success = 1
 
-    while True: 
-        # vidObj object calls read 
-        # function extract frames 
-        success, image = vidObj.read() 
+    #get total number of frames
+    total_frames = int(vidObj.get(cv2.CAP_PROP_FRAME_COUNT))
 
-        if not success:  # Break when no more frames
-            break
+    # Get the video frame rate
+    fps = vidObj.get(cv2.CAP_PROP_FPS)
 
-        # Saves the frames with frame-count 
-        cv2.imwrite(f"{path_header}/frame{num_frames}.jpg", image) 
+    # Calculate the number of frames to skip. the minus 1 is because we are saving one of the frames and skipping everything else
+    frames_to_skip = int(TR * fps) - 1
+
+    #get the number of TR
+    num_TR = total_frames / (fps * 1.5)
+
+    while success: 
+        # Read the next frame to save
+        success, image = vidObj.read()
+
+        # Save the current frame
+        cv2.imwrite(f"{path_header}/TR{current_TR}_frame{num_frames}.jpg", image)
+
+        # Print status
+        print(f"{current_TR} / {num_TR}")
+
+        # Skip frames
+        for _ in range(frames_to_skip):
+            success = vidObj.grab()  # Grab frames without decoding
+            num_frames += 1
+            if not success:
+                break
 
         num_frames += 1
+        current_TR += 1
     
     print(num_frames)
 
